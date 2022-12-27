f73c6b0145b867b15049b1e3152b23441a0f8f92
Rohitas Bansal <rohitas@antiersolutions.com>
2022-12-27 11:58:37 +0530

    Raw Pointers

---
+++ raw_pointer.rs
@@ -0,0 +1,26 @@
+#[allow(dead_code)]
+#[derive(Debug, Copy, Clone)]
+struct A<'text> {
+    foo: &'text str,
+    baz: i32,
+}
+
+fn ptr_to_struct(p: *const A) {
+    let b: A = unsafe { *p }; // main part of this example!
+    println!("{:#?}", b);
+}
+
+fn ptr_to_struct_2(p: usize) {
+    let b = unsafe { *(p as *const A) }; // main part of this example!
+    println!("{:#?}", b);
+}
+
+fn main() {
+    let a = A {
+        foo: "Hello",
+        baz: 2000,
+    };
+
+    ptr_to_struct(&a);
+    ptr_to_struct_2(&a as *const _ as usize);
+}
