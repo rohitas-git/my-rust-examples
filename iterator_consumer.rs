9c53079f7749435226783f9e64fdbdd9cd679e7d
Rohitas Bansal <rohitas@antiersolutions.com>
2023-02-01 12:16:49 +0530

    pending: min_max iterator consumer

---
+++ iterator_consumer.rs
@@ -0,0 +1,117 @@
+
+
+fn main(){
+    // ANY
+    iterator_consumers::any::determin_a_char_with_any("Hello World", 'o');
+    iterator_consumers::any::determine_negative_with_any();
+    // ALL
+    iterator_consumers::all::determine_positive_with_all();
+    // COUNT
+    iterator_consumers::count::number_with_count();
+    // SUM
+    iterator_consumers::sum::ex_sum();
+    iterator_consumers::sum::ex_sum2();
+    iterator_consumers::sum::ex_sum3();
+
+}
+
+/// * Iterator Consumer:
+/// 
+/// * Every function that is applied to an iterator but does not return an iterator is called “iterator consumer”, 
+/// because it gets data from an iterator but does not put them into another iterator, 
+/// and so it “consumes” data, instead of “adapting” data.
+/// 
+pub mod iterator_consumers{
+
+    /// * Any:
+    /// 
+    /// The any function, which is in the standard library, is applied to an iterator. 
+    /// * Its purpose is determining if a Boolean function (aka “predicate”) is true for any item produced by the iterator.
+    /// 
+    /// The any function must be applied to an iterator and must receive a closure as an argument. 
+    /// It applies that closure to every item received from the iterator, 
+    /// and it returns true as soon as the closure returns true on an item, 
+    /// or returns false if the closurereturns false for all the items.
+    /// 
+    pub mod any{
+        fn determin_a_char(s:&str,ch:char){
+            // let s = "Hello, world!";
+            // let ch = 'R';
+            let mut contains = false;
+            for c in s.chars() {if c == ch {contains = true;}}
+            print!("\"{}\" {} '{}'.",s,if contains {"contains"} else {"does not contain"}, ch);
+        }
+        pub fn determin_a_char_with_any(s:&str,ch:char){
+            // let s = "Hello, world!";
+            // let ch = 'R';
+            println!("\"{}\" {} '{}'.",s, if s.chars().any(|c| c == ch) {"contains"} else {"does not contain"}, ch);
+        }
+        pub fn determine_negative_with_any(){
+            print!("{} ", [45, 8, 2, 6].iter().any(|n: &i32| -> bool { *n < 0 }));
+            println!("{} ", [45, 8, -2, 6].iter().any(|n: &i32| -> bool { *n < 0 }));
+        }
+    }
+    /*
+        Notice that while 
+        * the any function means a repeated application of the or logical operator, 
+        * the all function means a repeated application of the and logical operator.
+
+        Notice also that, following logical rules, if the iterator produces no items, 
+        the any function returns false for any closure, and the all function returns true for any closure.
+    */
+    pub mod all{
+        pub fn determine_positive_with_all(){
+            print!("{} ", [45, 8, 2, 6].iter().all(|n: &i32| -> bool { *n > 0 }));
+            println!("{} ", [45, 8, -2, 6].iter().all(|n: &i32| -> bool { *n > 0 }));
+        }
+    }
+    /// * The count iterator consumer does not get any argument, and it always returns a usize value.
+    pub mod count{
+        fn number_with_len(){
+            // When Iterating for Array, Slice, Vector ... Use Len function of such objects
+            // as it is the simplest and fastest way to get their lengths.
+        }
+        /// But if you want to know how many characters there are in a string, 
+        /// you must scan it all, 
+        /// because the number of chars comprising a string is not stored anywhere, unless you did it.
+        pub fn number_with_count(){
+            let s = "€èe"; 
+            println!("char_count:{} string_len:{}", s.chars().count(), s.len());
+        }
+    }
+    /// * sum: add all the items in iterator to get a single value
+    /// 
+    /// sum iterator consumer does not get arguments. 
+    /// Yet, it requires a type parameter in angle brackets. ==> Type of returned number/value
+    /// 
+    /// * the sum function is applicable only to iterators that produce addable items. 
+    /*
+        Notice that while the count function was applicable to any iterator, 
+        the sum function is applicable only to iterators that produce addable items. 
+        The statement [3.4].iter().sum::<f64>(); is valid, 
+        while the statement [true].iter().sum::<bool>() is illegal,
+        because it is not allowed to sum Booleans.
+    */
+    pub mod sum{
+        pub fn ex_sum(){
+            println!("{}", [45, 8, -2, 6].iter().sum::<i32>());
+        }
+        pub fn ex_sum2(){
+            let s: i32 = [45, 8, -2, 6].iter().sum();
+            print!("{}", s);
+        }
+        pub fn ex_sum3(){
+            let s: u32 = [].iter().sum();
+            print!("{}", s);
+        }
+    }
+    /// If an iterator produces values that can be compared with one another, 
+    /// it is possible to get the minimum or the maximum of those values. But there is a problem: the empty sequences.
+    /// 
+    pub mod min_max{
+        
+    }
+}
+
+pub mod lazy_processing_iterator_chains{
+}
